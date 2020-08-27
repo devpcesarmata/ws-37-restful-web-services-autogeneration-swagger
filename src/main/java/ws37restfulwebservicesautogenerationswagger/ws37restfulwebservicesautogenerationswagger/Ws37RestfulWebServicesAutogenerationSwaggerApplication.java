0c0bf2cbdab6dd0e9ee22df8cadf94eff466dc65
César <dev.pcesarmata@gmail.com>
2020-08-27 09:38:21 -0500

    init commit

---
+++ src/main/java/ws37restfulwebservicesautogenerationswagger/ws37restfulwebservicesautogenerationswagger/Ws37RestfulWebServicesAutogenerationSwaggerApplication.java
@@ -0,0 +1,13 @@
+package ws37restfulwebservicesautogenerationswagger.ws37restfulwebservicesautogenerationswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws37RestfulWebServicesAutogenerationSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws37RestfulWebServicesAutogenerationSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws37restfulwebservicesautogenerationswagger/ws37restfulwebservicesautogenerationswagger/Ws37RestfulWebServicesAutogenerationSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ws37restfulwebservicesautogenerationswagger.ws37restfulwebservicesautogenerationswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws37RestfulWebServicesAutogenerationSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
